e9ce9c4ad4f39e61a95053be58e1061138bb6b5a
Hirokazu Chiba <hchiba1@gmail.com>
2021-08-13 17:39:49 +0900

    Format sites/main.js

--- sites/main.js
+++ sites/main.js
@@ -3,15 +3,15 @@ let byProgram = false, editor, outputArea;
 let timerId;
 
 function reformat(event, ui) {
-  try{
-    outputArea.setValue(spfmt.reformat(editor.getValue(), q('#indent-depth-input').value ));
-  } catch(e) {
+  try {
+    outputArea.setValue(spfmt.reformat(editor.getValue(), q('#indent-depth-input').value));
+  } catch (e) {
     console.log(e);
-    toastr.error('', 'SyntaxError', {preventDuplicates: true})
+    toastr.error('', 'SyntaxError', { preventDuplicates: true });
   }
 }
 
-function  onChanged(delta) {
+function onChanged(delta) {
   if (!byProgram) {
     clearTimeout(timerId);
     timerId = setTimeout(reformat, 500);
@@ -23,7 +23,7 @@ function  onChanged(delta) {
 editor = CodeMirror.fromTextArea(q('#sparql-input'), {
   lineNumbers: true,
   viewportMargin: Infinity,
-  lineWrapping: true,
+  lineWrapping: true
 });
 
 outputArea = CodeMirror.fromTextArea(q('#formatted-input'), {
@@ -39,7 +39,6 @@ outputArea.setSize('100%', '100%');
 
 editor.on('change', onChanged);
 
-
 q('#indent-depth-input').addEventListener('change', onChanged);
 
 q('#query-select').addEventListener('change', (event) => {
@@ -51,28 +50,26 @@ q('#query-select').addEventListener('change', (event) => {
       editor.setValue(response.data);
     });
   }
-})
-
+});
 
 q('#copy-button').addEventListener('click', () => {
   navigator.clipboard.writeText(outputArea.getValue());
-  toastr.success('', 'Copied to clipboard', {preventDuplicates: true, fadeOut: 200, timeOut: 2000})
+  toastr.success('', 'Copied to clipboard', { preventDuplicates: true, fadeOut: 200, timeOut: 2000 });
 });
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function (event) {
   let url = `https://api.github.com/repos/sparqling/sparql-formatter/contents/sparql11-query`;
   axios.get(url).then((response) => {
-
-    const selectNode = q("#query-select");
+    const selectNode = q('#query-select');
     selectNode.innerHTML = '';
 
-    let firstOption = document.createElement("option");
+    let firstOption = document.createElement('option');
     firstOption.innerText = '';
     selectNode.appendChild(firstOption);
 
-    for(let object of response.data) {
-      if(object.name.endsWith('.rq')) {
-        let option = document.createElement("option");
+    for (let object of response.data) {
+      if (object.name.endsWith('.rq')) {
+        let option = document.createElement('option');
         option.innerText = object.name;
         selectNode.appendChild(option);
       }
