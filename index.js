ccc30a19ceee294fa4e724d2983a0a01b44ec7e6
Hirokazu Chiba <hchiba1@gmail.com>
2023-01-25 16:17:59 +0900

    Fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const parser = require('lib/parser');
-const formatter = require('lib/formatter.js');
+const parser = require('./lib/parser.js');
+const formatter = require('./lib/formatter.js');
 
 module.exports = (sparql, indentDepth = 2) => {
   return formatter.format(parser.parse(sparql), indentDepth);
