a2eb83b3faa6e0b2663cf6b370399acab8e4b6da
Hirokazu Chiba <hchiba1@gmail.com>
2023-02-01 18:25:18 +0900

    Remove js/

--- js/_formatter.js
+++
@@ -1,5 +0,0 @@
-spfmt = (sparql, indentDepth = 2) => {
-  const parser = require('../lib/parser');
-  const formatter = require('../lib/formatter.js');
-  return formatter.format(parser.parse(sparql), indentDepth);
-};
