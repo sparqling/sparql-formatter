8acbf2dd887f7836a845f7de526408ef339f31fe
Hirokazu Chiba <hchiba1@gmail.com>
2023-02-01 17:52:38 +0900

    Install webpack as bundler

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const parser = require('./lib/parser.js');
 const formatter = require('./lib/formatter.js');
 
-module.exports = (sparql, indentDepth = 2) => {
+spfmt = (sparql, indentDepth = 2) => {
   return formatter.format(parser.parse(sparql), indentDepth);
 };
+
+module.exports = spfmt;
