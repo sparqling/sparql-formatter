ad788a4004d87353b840f57e021529ea4e4e2991
Hirokazu Chiba <hchiba1@gmail.com>
2023-02-06 11:15:37 +0900

    Update index.html and sites/main.js for textarea sizes

--- sites/main.js
+++ sites/main.js
@@ -1,5 +1,4 @@
-const q = document.querySelector.bind(document);
-let byProgram = false, editor, outputArea;
+let byProgram = false;
 let timerId;
 
 function reformat(event, ui) {
@@ -60,23 +59,6 @@ function onChanged(delta) {
     byProgram = false;
   }
 }
-
-editor = CodeMirror.fromTextArea(q('#input-sparql'), {
-  lineNumbers: true,
-  viewportMargin: Infinity,
-  lineWrapping: true
-});
-
-outputArea = CodeMirror.fromTextArea(q('#formatted-sparql'), {
-  lineNumbers: true,
-  viewportMargin: Infinity,
-  lineWrapping: true,
-  readOnly: true
-});
-
-editor.setSize('100%', '100%');
-
-outputArea.setSize('100%', '100%');
 
 editor.on('change', onChanged);
 
