bbb950343a0d700cc4b5edb531d9a74b02e1aa04
Roberto Guido <info@madbob.org>
2024-04-03 15:03:14 +0200

    fix variable definition
    Bundling this library with Vite, the missing `spfmt` variable initialization triggers an error.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const parser = require('./lib/parser.js');
 const formatter = require('./lib/formatter.js');
 
-spfmt = (sparql, indentDepth = 2) => {
+const spfmt = (sparql, indentDepth = 2) => {
   return formatter.format(parser.parse(sparql), indentDepth);
 };
 
