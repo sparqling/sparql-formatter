d63f5e8c64675668ad5366cea999382477f267ba
Hirokazu Chiba <hchiba1@gmail.com>
2024-12-22 18:52:58 +0900

    Update src/index.js using parser-compact.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import { parse } from './parser.js';
+import { parse as parseCompact } from './parser-compact.js';
 import { format } from './formatter.js';
 
-export const spfmt = (sparql, indentDepth = 2) => {
-  return format(parse(sparql), indentDepth);
+export const spfmt = (sparql, indentDepth = 2, compactMode = false) => {
+  if (compactMode) {
+    return format(parseCompact(sparql), indentDepth);
+  } else {
+    return format(parse(sparql), indentDepth);
+  }
 };
 
 if (typeof window !== 'undefined') {
